15ec07e760bf119f1ddd5685d5e7592cef25f000
yohandry10 <yohandrychirinos1@gmail.com>
2024-11-30 02:21:56 -0500

    12

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,6 +32,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/forum', forumRoutes);
 app.use('/api/profile', profileRoutes); // Añade la ruta de perfil
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
